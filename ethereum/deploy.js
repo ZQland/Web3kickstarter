f08fa01c123e283a8e98d7d6a5d292f3e888f6cc
ZQland <67606113+ZQland@users.noreply.github.com>
2023-02-07 22:39:16 -0500

    Add files via upload

---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'stadium nest grid target until age verb neither badge ladder current indoor',
+  'https://goerli.infura.io/v3/2fc1de650c854c3fae604367fd69a65f'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
