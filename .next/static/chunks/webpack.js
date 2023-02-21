faa121590638fef3fa0ba1e06c1a949ca91268ef
Ziquan Deng <ziquandeng@Ziquans-MacBook-Air.local>
2023-02-21 14:24:12 -0500

    added form for new request

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f2f81fb1b353283"; }
+/******/ 		__webpack_require__.h = function() { return "166913535f1583c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,8 +1,65 @@
 import React, { Component } from 'react';
+import { Button, Form, Input } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import { Link, Router } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 class RequestNew extends Component {
+    state = {
+        value: '',
+        description: '',
+        recipient: ''
+    }
+
+
+    static async getInitialProps (props) {
+        const { address } = props.query;
+
+        return { address };
+    }
+
+
     render() {
-        return <h3>Create a Request</h3>;
+        return (
+            <Layout>
+                <h3>Create a Request</h3>
+                <Form>
+                    <div class="field">
+                        <label>
+                            Description
+                        </label>
+                        <Input 
+                            value={this.props.description}
+                            onChange={event => this.setState({ description: event.target.value })}
+                        />
+                    </div>
+
+                    <div class="field">
+                        <label>
+                            Value in Ether
+                        </label>
+                        <Input 
+                            value={this.props.value}
+                            onChange={event => this.setState({ value: event.target.value})}
+                        />
+
+                    </div>
+
+                    <div class="field">
+                        <label>
+                            Recipient
+                        </label>
+                        <Input 
+                            value={this.props.recipient}
+                            onChange={event => this.setState({ recipient: event.target.value})}
+                        />
+
+                    </div>
+                    <Button primary>Create Request!</Button>
+                </Form>
+            </Layout>
+        );
     }
 }
 
